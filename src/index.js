752f5702657318cd9ed17efbc2bb3f4982e60cf4
AntonKilk <ant.kilk@gmail.com>
2019-12-01 14:50:38 +0200

    add ramda

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let star = <h1>{'\u2606'}</h1>
+let starRepeat = R.repeat(star, 3)
 
+ReactDOM.render(star, document.getElementById('root'))
